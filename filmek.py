bd5780362a8374706bc5a050e393beb36fd55125
AlexandraLukacs <lszandika15@gmail.com>
2024-01-18 09:20:49 +0100

    feladat bef

--- filmek.py
+++ filmek.py
@@ -14,16 +14,42 @@ def fajlbeolv():
     return film_lista
 
 def legrovidebb_film_cim(lista):
-    min: int= 9999999999
     min_index=0
-
+    min: int= lista[min_index].perc
     for i in range(0, len(lista), 1):
         if lista[i].perc < min:
             min= lista[i].perc
             min_index=i
-        return min_index
+    return min_index
     
 
 def percfilm110(lista):
-    """for i in range(0, len(lista), 1):
-        if lista[i]"""
+    db: int= 0
+    for i in range(0, len(lista), 1):
+        if lista[i].perc >= 110:
+            db += 1
+    return db
+
+
+def szinesz_film(lista):
+    szinesz: str= str(input("Adjon meg egy színész nevet: "))
+    van: int= 0
+    for i in range(0, len(lista), 1):
+        if lista[i].foszereplo == szinesz:
+            print(f"Pár film ajánlás: {lista[i].cim}")
+            van = 1
+    if van == 0:
+        print("Nincs ilyen nevű színséz")
+    return szinesz
+
+
+def kiir(lista, szinesz):
+    fajl=open("film2.txt", "w", encoding="utf-8")
+    van: int= 0
+    for i in range(0, len(lista), 1):
+        if lista[i].foszereplo == szinesz:
+            fajl.write(f"Pár film ajánlás: {lista[i].cim}\n")
+            van = 1
+    if van == 0:
+        fajl.write("Nincs ilyen nevű színséz")
+    fajl.close()
--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import filmek
 lista=filmek.fajlbeolv()
 legrovfilmcim=filmek.legrovidebb_film_cim(lista)
 print(f"A legrövidebb film címe: {lista[legrovfilmcim].cim}")
+perc110=filmek.percfilm110(lista)
+print(f"{perc110} db legalább 110 perces film van")
+szineszesfilm=filmek.szinesz_film(lista)
+filmek.kiir(lista, szineszesfilm)
